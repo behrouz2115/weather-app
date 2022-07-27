2775081a0a0bfc2900204851d0d2c2b3412ae29a
Behrouz Rozmeh <behrouz.rozmeh@gmail.com>
2022-07-27 20:23:02 +0900

    test

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ form.addEventListener("submit", e => {
             const { main, name, sys, weather } = data;
             const icon = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0]["icon"]}.svg`
             const li = document.createElement("li");
-            li.classList.add("city"); 
+            li.classList.add("city");
             const markup = `
             <h2 class='city-name' data-name=${name},${sys.country}>
                 <span>${name}</span>
